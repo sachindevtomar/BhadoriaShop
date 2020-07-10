93c32830a107fb20a4ee72b63e33a60b6cabf580
Sachin Dev Tomar <tomarsachindev@gmail.com>
2020-07-11 02:11:11 +0530

    Added Login and Sign up layouts

--- app/src/main/java/com/grocery/bhadoriashop/MainActivity.java
+++ app/src/main/java/com/grocery/bhadoriashop/MainActivity.java
@@ -7,13 +7,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
 import com.grocery.bhadoriashop.Fragments.HomeFragment;
+import com.grocery.bhadoriashop.Fragments.LoginFragment;
 import com.grocery.bhadoriashop.Fragments.RefundPolicyFragment;
+import com.grocery.bhadoriashop.Fragments.RegisterFragment;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     DrawerLayout drawerLayout;
@@ -50,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
         switch (item.getItemId()){
             case R.id.home_menu :
                 fragmentManager = getSupportFragmentManager();
@@ -65,6 +69,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 fragmentTransaction.commit();
                 break;
 
+            case R.id.login_menu :
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, new LoginFragment());
+                fragmentTransaction.commit();
+                break;
+
+            case R.id.register_menu :
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_fragment, new RegisterFragment());
+                fragmentTransaction.commit();
+                break;
             default:
 
 
