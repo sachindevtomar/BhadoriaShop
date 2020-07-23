c055780a13e4fc934eebcf4fc9668b27a3f3102c
Sachin Dev Tomar <tomarsachindev@gmail.com>
2020-07-23 22:47:03 +0530

    Removed unwanted files and made some changes in cardview of admin product list

--- app/src/main/java/com/grocery/bhadoriashop/Fragments/ProductListFragment.java
+++ app/src/main/java/com/grocery/bhadoriashop/Fragments/ProductListFragment.java
@@ -2,14 +2,12 @@ package com.grocery.bhadoriashop.Fragments;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -19,17 +17,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.grocery.bhadoriashop.Adapter.AdminProductListViewHolder;
-import com.grocery.bhadoriashop.Adapter.CardAdapterAdmin;
 import com.grocery.bhadoriashop.Models.AdminProductList;
-import com.grocery.bhadoriashop.Models.Planet;
 import com.grocery.bhadoriashop.R;
 
 import java.util.ArrayList;
 
 public class ProductListFragment extends Fragment {
     private RecyclerView recyclerView;
-    private CardAdapterAdmin adapter;
-    private ArrayList<Planet> planetArrayList;
     FirebaseRecyclerAdapter firebaseRecyclerAdapter;
     FirebaseDatabase mFirebaseDatabase;
     DatabaseReference mRef;
@@ -51,11 +45,6 @@ public class ProductListFragment extends Fragment {
         recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-//        planetArrayList = new ArrayList<>();
-//        adapter = new CardAdapterAdmin(getActivity(), planetArrayList);
-        recyclerView.setAdapter(adapter);
-        // recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
-//        createListData();
         //send Query to FirebaseDatabase
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mRef = mFirebaseDatabase.getReference("Products");
@@ -129,27 +118,5 @@ public class ProductListFragment extends Fragment {
     public void onStop() {
         super.onStop();
         firebaseRecyclerAdapter.stopListening();
-    }
-
-    private void createListData() {
-        Planet planet = new Planet("Earth", 150, 10, 12750);
-        planetArrayList.add(planet);
-        planet = new Planet("Jupiter", 778, 26, 143000);
-        planetArrayList.add(planet);
-        planet = new Planet("Mars", 228, 4, 6800);
-        planetArrayList.add(planet);
-        planet = new Planet("Pluto", 5900, 1, 2320);
-        planetArrayList.add(planet);
-        planet = new Planet("Venus", 108, 9, 12750);
-        planetArrayList.add(planet);
-        planet = new Planet("Saturn", 1429, 11, 120000);
-        planetArrayList.add(planet);
-        planet = new Planet("Mercury", 58, 4, 4900);
-        planetArrayList.add(planet);
-        planet = new Planet("Neptune", 4500, 12, 50500);
-        planetArrayList.add(planet);
-        planet = new Planet("Uranus", 2870, 9, 52400);
-        planetArrayList.add(planet);
-        adapter.notifyDataSetChanged();
     }
 }
